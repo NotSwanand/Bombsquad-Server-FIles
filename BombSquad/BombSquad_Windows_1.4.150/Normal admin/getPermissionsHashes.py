c156103607a4f0e5323aaaa67e4bbb6a4a82eea9
NotSwanand <swanand264@gmail.com>
2025-03-27 22:54:26 +0530

    Added BombSquad folder

---
+++ BombSquad/BombSquad_Windows_1.4.150/Normal admin/getPermissionsHashes.py
@@ -0,0 +1,3 @@
+vipHashes = []
+adminHashes = []
+ownerHashes =[]
---
+++ BombSquad/UnlockPro.py
@@ -0,0 +1,10 @@
+#modBySobyDamn
+from bsUtils import *
+import bsUtils
+bs.screenMessage("Pro Unlocked By ZEN-OH SAMA")
+def _000OOO0():
+    return True
+def _00000OOO():
+    return True
+bsUtils._haveProOptions = _000OOO0
+bsUtils._havePro = _00000OOO
